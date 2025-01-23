0e8a8f5513fc8420ce08ca8b57b7198142a3d508
jjhonny <ojhonny1@hotmail.com>
2025-01-23 10:25:39 -0400

    Add product controller and model with GET endpoint for products

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-product-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
+
+	ProductController := controller.NewProductController()
 
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductController.GetProducts)
 
 	server.Run(":8080")
 }
